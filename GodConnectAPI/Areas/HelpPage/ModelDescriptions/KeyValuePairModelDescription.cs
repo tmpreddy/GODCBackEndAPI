ac75791ea229d7e5da736c39095c0b0ae825ea5c
mahipalreddyt <54412295+mahipalreddyt@users.noreply.github.com>
2019-08-23 23:50:36 +0530

    mahicomit
    first

---
+++ GodConnectAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace GodConnectAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
